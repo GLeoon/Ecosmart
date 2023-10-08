6cf897bf7015d03659911b2436e295183d16ad1a
Guilherme Leon <guilherme.silva@predito.com.br>
2023-10-08 20:10:59 -0300

    Finalizando desenvolvimento da tabela

--- frontend/src/components/Tabelas.tsx
+++ frontend/src/components/Tabelas.tsx
@@ -1,4 +1,6 @@
 import {
+  Box,
+  Button,
   Table,
   TableBody,
   TableCell,
@@ -6,40 +8,86 @@ import {
   TableHead,
   TableRow,
 } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { useState } from "react";
+import ResponsiveDialog from "./Dialogo";
 
 interface IProps {
-  attributes: { [key: string]: string };
-  data: { [key: string]: string }[];
+  data?: { [key: string]: any }[];
+  remove: (id: number) => void;
 }
 
-export function Tabelas({ attributes, data }: IProps) {
+export function Tabelas({ data, remove }: IProps) {
+  const [open, setOpen] = useState(false);
+  const [selected, setSelected] = useState<any>();
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <TableContainer>
-      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-        <TableHead>
-          <TableRow>
-            {Object.keys(attributes).map((titles) => (
-              <TableCell align={titles == "name" ? "left" : "right"}>
-                {titles}
-              </TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              {Object.keys(attributes).map((inf) => (
-                <TableCell align={inf == "name" ? "left" : "right"}>
-                  {row.inf}
+    <Box
+      sx={{
+        height: "auto",
+        backgroundColor: "white",
+        borderRadius: "7px",
+        margin: "20px",
+      }}
+    >
+      <ResponsiveDialog
+        inf={selected}
+        open={open}
+        handleClose={handleClose}
+        remove={remove}
+      />
+      <TableContainer>
+        <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+          {data ? (
+            <TableHead>
+              <TableRow>
+                {Object.keys(data[0]).map((titles) => (
+                  <TableCell key={titles} sx={{ fontWeight: "bold" }}>
+                    {titles}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+          ) : null}
+          <TableBody>
+            {data?.map((row) => (
+              <TableRow
+                key={row.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                {Object.keys(data[0]).map((inf) => (
+                  <TableCell key={`${inf}${row}`}>{row[inf]}</TableCell>
+                ))}
+                <TableCell>
+                  <Button
+                    sx={{ color: "black" }}
+                    onClick={() => console.log("abre popup")}
+                  >
+                    <EditIcon />
+                  </Button>
                 </TableCell>
-              ))}
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+                <TableCell>
+                  <Button
+                    sx={{ color: "black" }}
+                    onClick={() => (handleClickOpen(), setSelected(row))}
+                  >
+                    <DeleteIcon />
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
   );
 }
